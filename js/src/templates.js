f3f2a7bf9e40394a83fc6b5ada5ed16111b4546e
Nils P. Ellingsen <nils@deviant.no>
2013-03-20 22:58:21 +0100

    temp fork: mixing things up
    probably the wrong way™

--- js/src/templates.js
+++ js/src/templates.js
@@ -1,1 +1,1 @@
-var tmpl = {"main" : '<div class="hero-unit"><h1>Patsy Boilerplate</h1><p>Because I'm lazy</p></div>', "done": "true"};
+var tmpl = {"main" : '<div class="hero-unit"><h1>Patsy Boilerplate</h1><p>Because I am lazy :)</p></div>', "done": "true"};
