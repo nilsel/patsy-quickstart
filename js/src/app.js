50d42627a80b6cb873bf083bfb2301a95525628d
Nils P. Ellingsen <nils@deviant.no>
2013-08-07 23:05:37 +0200

    ALL THE THINGS

--- js/src/app.js
+++ js/src/app.js
@@ -4,39 +4,16 @@
   var main = function(){
 
     var template = Handlebars.compile($.tmpl.main);
-    var context = {'text': 'Hello from handlebars+javascript'};
+    var context = {'text': '\'Don\'t be afraid of making mistakes. If you hide your ignorance, no one will hit you and you\'ll never learn.'};
     var html    = template(context);
 
     $('#viewport').prepend(html);
 
   };
 
-  var parseTweets = function(json){
-
-    var template = Handlebars.compile($.tmpl.tweets);
-    var context = {tweet: json.results};
-    var html = template(context);
-
-    $('#tweets').append(html);
-  };
-
-  var getTweets = function(){
-
-    $.ajax({
-      //url: '/twitter/search.json',
-      url: '/static/testdata.json',
-      data: {'q': 'from:horse_js'},
-      dataType: 'json',
-      type: 'get',
-      success: parseTweets
-    });
-
-  };
-
 
   $(document).ready(function(){
     main();
-    getTweets();
   });
 
 })(jQuery);
